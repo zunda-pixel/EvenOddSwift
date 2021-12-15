a06b2f32f2ffcd93bc89167eeb6dae7525a96cc5
h-yazawa <h-yazawa@ostechnology.co.jp>
2021-12-15 19:01:53 +0900

    メインの中身を実装

--- Sources/EvenOddSwift/EvenOddSwift.swift
+++ Sources/EvenOddSwift/EvenOddSwift.swift
@@ -1,6 +1,24 @@
+import Foundation
+import FoundationNetworking
+
 @main
 struct EvenOddSwift {
     static func main() async {
-        print("Hello, world!")
+        let number = 10
+
+        do {
+            let isEven = try await isEven(number: number)
+            print("\(number) is \(isEven ? "even" : "odd")")
+        } catch {
+            print(error)
+        }
     }
+}
+
+func isEven(number: Int) async throws -> Bool {
+    let baseURL: URL = .init(string: "https://api.isevenapi.xyz/api/iseven/")!
+    let url: URL = baseURL.appendingPathComponent("\(number)")
+    let (data, _) = try await URLSession.shared.data(from: url)
+    let model = try JSONDecoder().decode(Model.self, from: data)
+    return model.iseven
 }
